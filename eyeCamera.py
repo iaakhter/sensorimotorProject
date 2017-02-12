bb62bcfbff35e943b387804a8babed56c80217a2
iaakhter <iaakhter@colby.edu>
2017-02-11 18:57:21 -0800

    added the capability so that the target moves horizontally and we can visually simulate the focus of the eye with a small red circle along the optical axis

--- eyeCamera.py
+++ eyeCamera.py
@@ -2,18 +2,19 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from numpy import *
+from math import *
+from time import time
 
 # References:
 #         https://noobtuts.com/python/opengl-introduction
 #         https://learnopengl.com/#!Getting-started/Camera
-
-width, height = 500, 400   
+#         https://gist.github.com/strife25/803118
 
 def setUpCamera(cameraPosition,cameraTarget,cameraUp):
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 	glMatrixMode(GL_PROJECTION)
 	glLoadIdentity()
-	gluPerspective(40.,1.,1.,40.)
+	#gluPerspective(400.,1.,1.,400.)
 	#set up view matrix
 	glMatrixMode(GL_MODELVIEW)
 	glLoadIdentity()
@@ -25,26 +26,53 @@ def setUpCamera(cameraPosition,cameraTarget,cameraUp):
 			  cameraUp[0],cameraUp[1],cameraUp[2])
 
 def drawTarget(x, y, z, width, height):
+	glColor3f(0.0, 0.0, 1.0)
 	glBegin(GL_QUADS)
-	glVertex3f(x, y, z)                               
-	glVertex3f(x+width, y, z)                      
-	glVertex3f(x+width, y+height, z)            
-	glVertex3f(x, y+height, z)                   
+	glVertex3f(x-width/2.0, y-height/2.0, z)                               
+	glVertex3f(x+width/2.0, y-height/2.0, z)                      
+	glVertex3f(x+width/2.0, y+height/2.0, z)            
+	glVertex3f(x-width/2.0, y+height/2.0, z)                 
 	glEnd()
-	
 
+def drawFilledCircle(x, y, radius):
+	triangleAmount = 20 # of triangles used to draw circle
+	twicePi = 2.0 * pi
+	
+	glBegin(GL_TRIANGLE_FAN)
+	glVertex3f(x, y,0.0) # center of circle
+	for i in range(triangleAmount):
+		glVertex3f(x + (radius * cos(i *  twicePi / triangleAmount)), y + (radius * sin(i * twicePi / triangleAmount)),0.0)
+	glEnd()
+
+#Draw a circle representing the focus of the eye (along the optical axis)
+def perceivedTarget():
+	glColor3f(1.0, 0.0, 0.0)
+	drawFilledCircle(0,0,0.01)
+
+#callback function for opengl
 def setUpSystem():
-	cameraPosition = array([0.0,0.0,3.0])
+	startTime = time()%2.0
+	#radius = 0.1
+	#camX = sin(startTime)*radius
+	#camZ = cos(startTime)*radius
+	cameraPosition = array([0.0,0.0,1.0])
 	cameraTarget = array([0.0,0.0,0.0])
 	cameraUp = array([0.0,1.0,0.0])
+	#cameraPosition[0] = camX
+	#cameraPosition[2] = camZ
 	setUpCamera(cameraPosition,cameraTarget,cameraUp)
-	glColor3f(0.0, 0.0, 1.0)
-	drawTarget(0.0,0.0,0.0,200,100)
+	
+	#Move the target horizontally
+	xPosition = -1+startTime
+	drawTarget(xPosition,0.0,0.0,0.5,0.25)
+	perceivedTarget()
 	glutSwapBuffers()
 	print "done setting up camera"
 
 def main():
 	# initialization
+	width, height = 500, 400
+	startTime = time()
 	window = 0                                             # glut window number                            
 	glutInit()                                             # initialize glut
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
