b729caa3debcc0b46bccb69148ff7647207e8f7f
Ariadna <ariadna.estrada42@gmail.com>
2017-03-28 16:21:28 -0700

    updated model and training file

--- processImages.py
+++ processImages.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from PIL import Image
 
 def convertImageToArray (numberOfExamples, imagePath):
     #Get dimensions
@@ -17,6 +18,25 @@ def convertImageToArray (numberOfExamples, imagePath):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray = gray/255.0
         X[i] = np.reshape(gray, (1, n*d))
+        
+    return X
+
+def convertImageToArrayColor (numberOfExamples, imagePath):
+    #Get dimensions
+    firstImageName = imagePath + str(0) +'.png'
+    img = cv2.imread(firstImageName)
+    [n,d,t] = img.shape
+    
+    
+    X = np.zeros((numberOfExamples, n*d*t))
+
+    
+    for i in range(numberOfExamples):
+        imageName = imagePath + str(i) +'.png'
+        img = cv2.imread(imageName)
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # gray = gray/255.0
+        X[i] = np.reshape(img, (1, n*d*t))
         
     return X
 
@@ -51,10 +71,37 @@ def convertLabelToArray (numberOfExamples, labelPath):
     trainingLabelsFile.close()
     return y
 
+def resizeImages (numberOfExamples, filePath, savePath):
+    for i in range(numberOfExamples):
+        currImage = filePath + str(i) +'.png'
+        img = Image.open(currImage)
 
+        resizedImage = img.resize((50,50), Image.BICUBIC)
+
+        imageName = savePath + str(i) + ".png"
+        resizedImage.save(imageName, 'PNG')
+
+def standardizeCols(M, *args):
+        stdM = np.zeros(M.shape, dtype =np.float32)
+        if (len(args) > 0):
+            mu =  args[0]
+            std_dev = args[1]
+        else:
+        #if mu and sigma are omitted, compute from M
+            mu = np.mean(M, axis=0)
+            std_dev = np.std(M, axis=0)
+
+        stdM = (M - mu) / std_dev
+
+        return stdM, mu, std_dev
 if __name__ == "__main__":
    #test run
     #X = convertImageToArray(234, 'trainingData/trainingImages/image')
-    #y = convertLabelToArray(234, 'trainingData/trainingLabel.txt')
-    X = constructXFromTargetFocusLocations(1,"trainingData/trainingFeature.txt")
-    print X
+    # y = convertLabelToArray(500, 'trainingData/trainingLabel.txt')
+    # X = constructXFromTargetFocusLocations(1,"trainingData/trainingFeature.txt")
+    resizeImages (2135,"trainingData/trainingImages/image", "trainingData/resizedImages/image")
+    # X = convertImageToArrayColor(1, "trainingData/resizedImages/image")
+    # stdy, muTrain, sigmaTrain = standardizeCols(y)
+    # print stdy, muTrain, sigmaTrain
+    # print X.shape
+    # print np.max(X)
--- train_example.py
+++ train_example.py
@@ -1,56 +1,71 @@
 import model_example
 import tensorflow as tf
 import processImages
+import numpy as np
 
-#globals
-epochs_completed = 0
-index_in_epoch = 0
+class trainCNN:
+    def __init__(self):
+        # Load Training Data
+        imagePath ="trainingData/trainingFeature.txt"
+        labelPath = "trainingData/trainingLabel.txt"
+        self.numOfExamples = 2000
 
-# Load Training Data
-imagePath ="trainingData/trainingFeature.txt"
-labelPath = "trainingData/trainingLabel.txt"
-numOfExamples = 2000
+        traningImagesPath = "trainingData/resizedImages/image"
 
-# data = Data()
-xTrain = processImages.constructXFromTargetFocusLocations(numOfExamples, imagePath)
-yTrain = processImages.convertLabelToArray(numOfExamples, labelPath)
+        #To train using centers, uncomment next two lines
+        xTrain = processImages.constructXFromTargetFocusLocations(self.numOfExamples, imagePath)
+        self.xTrain = np.reshape(xTrain, (self.numOfExamples, 1, 2, 1))
 
-# Start session
-sess = tf.InteractiveSession()
+        #To train using the 50x50 image in grey, uncomment the next two lines
+        # xTrain = processImages.convertImageToArray(self.numOfExamples, traningImagesPath)
+        # self.xTrain = np.reshape(xTrain, (self.numOfExamples, 50, 50, 1))
 
-# Learning Functions
-L2NormConst = 0.001
-train_vars = tf.trainable_variables()
-loss = tf.reduce_mean(tf.square(tf.subtract(model_example.y_, model_example.y))) + tf.add_n([tf.nn.l2_loss(v) for v in train_vars]) * L2NormConst
-train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+        yTrain = processImages.convertLabelToArray(self.numOfExamples, labelPath)
+        self.yTrain, self.muYTrain, self.stdYTrain = processImages.standardizeCols(yTrain)
 
-sess.run(tf.global_variables_initializer())
+    def train(self):
+        #batch variables
+        epochs_completed = 0
+        index_in_epoch = 0
+        # Start session
+        sess = tf.InteractiveSession()
+        # Learning Functions
+        L2NormConst = 0.001
+        train_vars = tf.trainable_variables()
+        loss = tf.reduce_mean(tf.square(tf.subtract(model_example.y_, model_example.y))) + tf.add_n([tf.nn.l2_loss(v) for v in train_vars]) * L2NormConst
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
-# Training loop variables
-epochs = 100
-batch_size = 50
-num_samples = numOfExamples
-step_size = int(num_samples / batch_size)
+        sess.run(tf.global_variables_initializer())
 
-for epoch in range(epochs):
-    for i in range(step_size):
-    	start = index_in_epoch
-    	index_in_epoch += batch_size
-    	if index_in_epoch >= numOfExamples:
-    		#Finished epoch
-    		epochs_completed += 1
-    		#Start new epoch
-    		start = 0
-    		index_in_epoch = batch_size
-    	end = index_in_epoch
-        batch = xTrain[start:end], yTrain[start:end]
+        # Training loop variables
+        epochs = 200
+        batch_size = 100
+        num_samples = self.numOfExamples
+        step_size = int(num_samples / batch_size)
 
-        train_step.run(feed_dict={model_example.x: batch[0], model_example.y_: batch[1], model_example.keep_prob: 0.8})
+        for epoch in range(epochs):
+            for i in range(step_size):
+                start = index_in_epoch
+                index_in_epoch += batch_size
+                if index_in_epoch >= self.numOfExamples:
+                    #Finished epoch
+                    epochs_completed += 1
+                    #Start new epoch
+                    start = 0
+                    index_in_epoch = batch_size
+                end = index_in_epoch
+                batch = self.xTrain[start:end], self.yTrain[start:end]
 
-        if i%10 == 0:
-          loss_value = loss.eval(feed_dict={model_example.x:batch[0], model_example.y_: batch[1], model_example.keep_prob: 1.0})
-          print("epoch: %d step: %d loss: %g"%(epoch, epoch * batch_size + i, loss_value))
+                train_step.run(feed_dict={model_example.x: batch[0], model_example.y_: batch[1], model_example.keep_prob: 0.8})
 
-# Save the Model
-saver = tf.train.Saver()
-saver.save(sess, "model.ckpt")
+                if i%10 == 0:
+                  loss_value = loss.eval(feed_dict={model_example.x:batch[0], model_example.y_: batch[1], model_example.keep_prob: 1.0})
+                  print("epoch: %d step: %d loss: %g"%(epoch, epoch * batch_size + i, loss_value))
+
+        # Save the Model
+        saver = tf.train.Saver()
+        saver.save(sess, "model.ckpt")
+
+if __name__ == "__main__":
+    modelTest = trainCNN()
+    modelTest.train()
